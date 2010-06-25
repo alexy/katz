7f2d8a08a56bf7594dd5ab70c661e35d90cf2e31
Alexy Khrabrov <deliverable@gmail.com>
2010-06-24 23:04:25 -0400

    load graph either from marshalled or from tokyo-json formats

--- sc.ml
+++ sc.ml
@@ -1,22 +1,32 @@
 open Batteries_uni
-open Binary_graph
 open Utils
 open Graph
 open Soc_run
 
+let suffix = String.ends_with
+
+let loadAnyGraph : string -> graph =
+  fun fileName ->
+    if suffix fileName "mlb" then
+      Binary_graph.loadData fileName
+    else
+    if suffix fileName "json.hdb" then
+      Tokyo_graph.fetchGraph fileName None (Some 10000)
+    else failwith "unrecognized graph file extension" 
+  
 let () = 
   let args = getArgs in
   match args with
   | drepsName::dmentsName::saveName::restArgs -> begin
     leprintfln "reading graph from %s and %s, saving dcaps in %s" drepsName dmentsName saveName;
     let maxDays = restArgs |> List.map int_of_string |> option_of_list in
-    let dreps : graph = loadData drepsName in
+    let dreps = loadAnyGraph drepsName in
     leprintfln "loaded %s, %d" drepsName (H.length dreps);
-    let dments : graph = loadData dmentsName in
+    let dments = loadAnyGraph dmentsName in
     leprintfln "loaded %s, %d" dmentsName (H.length dments);
     
     let {dcapsSG =dcaps} = socRun dreps dments {optSocRun with maxDaysSR= maxDays} in
     leprintfln "computed sgraph, now saving dcaps in %s" saveName;
-    saveData dcaps saveName
+    Binary_graph.saveData dcaps saveName
   end
   | _ -> leprintf "usage: sc dreps dments save-to"
