68b9d762c9c3f73c89793da212cea16a21ba29d6
Alexy Khrabrov <deliverable@gmail.com>
2010-07-16 02:06:34 -0400

    default is evaluated strictly, don't want extra creates

--- invert.ml
+++ invert.ml
@@ -10,10 +10,19 @@ let invert1 : graph -> graph = fun g ->
 	 (fun day reps -> 
 	    H.iter 
 	      (fun to' num -> 
-		 let to_days  = H.find_default res to' 
-		   (let x: adjlist = H.create 10 in H.add res to' x; x) in
-		 let to_reps  = H.find_default to_days day 
-		   (let x: reps = H.create 10 in H.add to_days day x; x) in
+		 let to_days  = match H.find_option res to' with
+		   | Some x -> x
+		   | _ -> let x: adjlist = H.create 10 in H.add res to' x; x in
+		 let to_reps  = match H.find_option to_days day with
+		   | Some x -> x
+		   | _ -> let x: reps = H.create 10 in H.add to_days day x; x in
 		 H.add to_reps from num) reps) days) g;
    res
-					    
+
+(*					    
+let invert2 : graph -> graph = fun g ->
+  let res = H.enum g in
+  Enum.map (fun (from, days) -> let days' = H.enum days in
+	      Enum.map (fun (day,reps) -> let reps' = H.enum reps in
+			  Enum.map (fun (to',num) -> (to',(day,from,num))
+ ...*)
