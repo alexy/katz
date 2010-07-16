e002292a9d3f27db4be0d79f47a88cc800806337
Alexy Khrabrov <deliverable@gmail.com>
2010-07-16 01:23:42 -0400

    actually be the change

--- invert.ml
+++ invert.ml
@@ -10,8 +10,10 @@ let invert1 : graph -> graph = fun g ->
 	 (fun day reps -> 
 	    H.iter 
 	      (fun to' num -> 
-		 let to_days  = H.find_default res to' (H.create 10) in
-		 let to_reps  = H.find_default to_days day (H.create 10) in
+		 let to_days  = H.find_default res to' 
+		   (let x: adjlist = H.create 10 in H.add res to' x; x) in
+		 let to_reps  = H.find_default to_days day 
+		   (let x: reps = H.create 10 in H.add to_days day x; x) in
 		 H.add to_reps from num) reps) days) g;
    res
 					    
