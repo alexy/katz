f62116b71cf09e6ede83b87d59cec6b9e0029375
Alexy Khrabrov <deliverable@gmail.com>
2010-12-22 16:02:26 -0500

    and add that simfony too

---
+++ dosimf.ml
@@ -0,0 +1,36 @@
+open Common
+open Binary_graph
+
+let () =
+  let args = getArgs in
+  match args with
+    | duvalsName::dstartsName::denumsName::erepsName::restArgs -> 
+      begin
+      leprintfln "reading duvals from %s, dstarts from %s and dnums from %s, saving ereps in %s" 
+        duvalsName dstartsName denumsName erepsName;
+    
+      (* By_day.user_day_reals <=> Soc_run.dCaps *)
+      let udvals: By_day.user_day_reals = loadData duvalsName in
+      let dstarts: Dranges.starts       = loadData dstartsName in
+      let denums:  By_day.day_edgenums  = loadData denumsName in
+
+      let duvals = Some (By_day.dayUserReals udvals) in
+      
+      let ereps = 
+        match restArgs with
+        | [] -> 
+          Simulate.simulate dstarts denums ~duvals
+        | drepsName::day'::[] -> begin
+          let day = int_of_string day' in
+          leprintfln "based on %s through day %d" drepsName day;
+          let dreps: graph = loadData drepsName in
+          (* inverting is much slower than loading dments.mlb...
+             leprintfln "inverting...";
+             let dments = Invert.invert2 dreps in *)
+          let dreps_day = (dreps,day) in
+          Simulate.simulate ~dreps_day dstarts denums ~duvals
+        end
+        | _ -> failwith "dreps usage: dosim dstartsName denumsName erepsName drepsName day"
+      in saveData ereps erepsName
+      end
+  | _ -> failwith "usage: dosim dstartsName dnumsName erepsName [drepsName day]"
