8a519077199c7e0768595dd92688058dbfe12fb7
Alexy Khrabrov <deliverable@gmail.com>
2011-01-08 01:06:20 -0500

    generate dskews name from dcaps

--- sk.ml
+++ sk.ml
@@ -7,21 +7,24 @@ let by_mass = true
   
 let () = 
   let args = getArgs in
+  let (drepsName,restArgs) =
   match args with
-  | drepsName::saveName::restArgs -> begin
-    leprintfln "reading graph from %s, saving dcaps in %s" drepsName saveName;
-    let maxDays = restArgs |> List.map int_of_string |> option_of_list in
-    let (dreps,tLoad) = loadAnyGraph drepsName in
-    leprintfln "loaded %s, %d" drepsName (H.length dreps);
-    let dments = Invert.invert2 dreps in
-    let tInvert = Some "inverted dreps into dments, timing: " |> getTiming in
-    leprintfln "dments has length %d" (H.length dments);
-    let ({dcapsSG =dcaps; dskewsSG =dskews},tSocRun) = socRun dreps dments {optSocRun with maxDaysSR= maxDays; byMassSR= by_mass} in
-    leprintfln "computed sgraph, now saving dcaps in %s" saveName;
-    (* saveData dcaps saveName; *)
-    saveData dskews saveName;
-    let tSaving =  Some "saved dcaps timing: " |> getTiming in
-    let ts = List.rev (tSaving::tSocRun@[tInvert]@tLoad) in
-    printf "timings: %s\n" (show_float_list ts);
-  end
-  | _ -> leprintf "usage: sc drepsName dcapsName"
+  | drepsName::restArgs -> (drepsName,restArgs)
+  | _ -> failwith "usage: sk drepsName"
+  in
+  let skewName = "skew-"^drepsName in
+  leprintfln "reading graph from %s, saving dskews in %s" drepsName skewName;
+  let maxDays = restArgs |> List.map int_of_string |> option_of_list in
+  let (dreps,tLoad) = loadAnyGraph drepsName in
+  leprintfln "loaded %s, %d" drepsName (H.length dreps);
+  let dments = Invert.invert2 dreps in
+  let tInvert = Some "inverted dreps into dments, timing: " |> getTiming in
+  leprintfln "dments has length %d" (H.length dments);
+  let ({dcapsSG =dcaps; dskewsSG =dskews},tSocRun) = 
+    socRun dreps dments {optSocRun with maxDaysSR= maxDays; byMassSR= by_mass} in
+  leprintfln "computed sgraph, now saving dskews in %s" skewName;
+  (* saveData dcaps saveName; *)
+  saveData dskews skewName;
+  let tSaving =  Some "saved dskews timing: " |> getTiming in
+  let ts = List.rev (tSaving::tSocRun@[tInvert]@tLoad) in
+  printf "timings: %s\n" (show_float_list ts);
