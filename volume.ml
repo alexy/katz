08da3bc3932b2e178953a4c979e459e038e20a87
Alexy Khrabrov <deliverable@gmail.com>
2011-01-20 16:10:10 -0500

    debugging where totals tally assertion fails for mentions

--- volume.ml
+++ volume.ml
@@ -19,7 +19,14 @@ let bucket_volumes: By_day.day_user_ints -> Topsets.day_buckets -> bucket_volume
          res + (H.find_default dnums user 0)
        end userset 0
      end buckets in
-     assert (L.sum bucket_totals = totals.(day));
+     
+     (* assert (L.sum bucket_totals = totals.(day)); *)
+     let sum_buckets = L.sum bucket_totals in
+     begin match sum_buckets = totals.(day) with
+     | true -> ()
+     | false -> failwith (sprintf "sum bucket_totals, %d /= %d, denums total for the day"
+                sum_buckets totals.(day))
+     end;
      bucket_totals
   end bucks
   
