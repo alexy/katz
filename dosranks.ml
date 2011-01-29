6f4f147977a4329e712606acb86c621f2d478246
Alexy Khrabrov <deliverable@gmail.com>
2011-01-29 13:59:07 -0500

    use 1e-7 as minCap

--- dosranks.ml
+++ dosranks.ml
@@ -5,7 +5,7 @@ open Common
 let () =
   let args = getArgs in
   let minDaysO = Some 7 in
-  let minCap  = 1e-35 in
+  let minCap  = 1e-7 in
   let drepsName,dcapsName =
   match args with
     | drepsName::dcapsName::restArgs -> drepsName,dcapsName
