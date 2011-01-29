a32f0140ea76bb1c9551264c1d6b76f72fcc51c4
Alexy Khrabrov <deliverable@gmail.com>
2011-01-29 16:17:14 -0500

    dosranks uses getopt to set minCap

--- dosranks.ml
+++ dosranks.ml
@@ -1,11 +1,21 @@
 (* compute starranks *)
    
 open Common
+open Getopt
+
+let minDaysO = Some 7
+let minCap = ref 1e-35
+let specs =
+[
+  ('c',"mincap",None,Some (fun x -> minCap := float_of_string x))
+]
 
 let () =
-  let args = getArgs in
-  let minDaysO = Some 7 in
-  let minCap  = 1e-7 in
+  let restArgsE = E.empty () in
+  let pushArg a = E.push restArgsE a in
+  parse_cmdline specs pushArg;
+  let args = L.of_enum restArgsE |> L.rev in
+
   let drepsName,dcapsName =
   match args with
     | drepsName::dcapsName::restArgs -> drepsName,dcapsName
@@ -13,13 +23,17 @@ let () =
   in  
 
   let starsName = "stars-"^dcapsName in
+  leprintfln "reading dreps from %s, dcaps from %s, storing stars in %s" drepsName dcapsName starsName;
+  match minDaysO with
+  | Some minDays -> leprintfln "applying minCap %f for maturities less than %d days" !minCap minDays
+  | _ -> leprintfln "not using maturity at all";
 
   let dreps: dreps = loadData drepsName in
   let dcaps: dcaps = loadData dcapsName in
   
   let dcapsh,startsh = Dcaps.dcaps_hash dcaps in
   let maturity = match minDaysO with
-  | Some minDays -> Some (startsh,minDays,minCap)
+  | Some minDays -> Some (startsh,minDays,!minCap)
   | _ -> None in
   let stars = Starrank.starrank dreps dcapsh maturity in
   
