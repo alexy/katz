763148788c7ff08adeb84f2c886eb12c1d243384
Alexy Khrabrov <deliverable@gmail.com>
2011-02-11 02:17:55 -0500

    new list to run

--- run-lreps.sh
+++ run-lreps.sh
@@ -9,5 +9,11 @@
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofcap fg5mf1c3wk ../ereps/dreps.mlb 21
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofcap fg5mf1c4wk ../ereps/dreps.mlb 28
 
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofmen fg5uf1m0
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofmen fg5uf1m1wk ../ereps/dreps.mlb 7
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofmen fg5uf1m2wk ../ereps/dreps.mlb 14
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofmen fg5uf1m3wk ../ereps/dreps.mlb 21
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofmen fg5uf1m4wk ../ereps/dreps.mlb 28
+
 for i in caps*; do ../../docranks.opt $i; done
 for i in aranks*; do ../../save_rbucks.opt $i; done
