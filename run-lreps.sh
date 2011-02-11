5a1d0c902e78190ae49095dbcf901ce7d1841e95
Alexy Khrabrov <deliverable@gmail.com>
2011-02-10 21:16:29 -0500

    new list to run, rerun mm without backup hopefully (filtering in fnumMents)

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,9 +1,4 @@
-../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofuni fg5mf1u0a
-../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofuni fg5mf1u1wk ../ereps/dreps.mlb 7
-../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofuni fg5mf1u2wk ../ereps/dreps.mlb 14
-../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofuni fg5mf1u3wk ../ereps/dreps.mlb 21
-../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofuni fg5mf1u4wk ../ereps/dreps.mlb 28
-
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofmen fg5mf1m0a
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofmen fg5mf1m1wk ../ereps/dreps.mlb 7
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofmen fg5mf1m2wk ../ereps/dreps.mlb 14
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glomen --fofmen fg5mf1m3wk ../ereps/dreps.mlb 21
