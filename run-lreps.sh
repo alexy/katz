e04100a93b2760ef378c255a45117b77af4fe8ed
Alexy Khrabrov <deliverable@gmail.com>
2011-02-12 00:45:31 -0500

    redu uu0

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,3 +1,4 @@
+../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofuni fg5uf1u0
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofuni fg5uf1u1wk ../ereps/dreps.mlb 7
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofuni fg5uf1u2wk ../ereps/dreps.mlb 14
 ../../sf.opt ../dstarts.mlb ../drnums/nr-dreps.mlb -j 0.5 -J 0.1 --glouni --fofuni fg5uf1u3wk ../ereps/dreps.mlb 21
@@ -31,7 +32,7 @@
 for i in caps*; do ../../docranks.opt $i; done
 for i in aranks*; do ../../save_rbucks.opt $i; done
 
-mv rbucks* rbucks
+mv rbucks-* rbucks
 cd rbucks
 
 for i in rbucks*; do ../../../doversets.opt ../../rbucks/rbucks-aranks-caps-dreps.mlb rbucks-aranks-caps-$i.mlb dreps-$i; done
