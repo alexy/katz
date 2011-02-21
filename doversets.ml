53fef5ffe691b21792d6a2ee160174a3060e2763
Alexy Khrabrov <deliverable@gmail.com>
2011-02-21 14:14:12 -0500

    save orates directly instead of osets, skip overy

--- doversets.ml
+++ doversets.ml
@@ -1,31 +1,55 @@
 open Common
 open Topsets
+open Getopt
+
+let oratesNow' = ref true
+let saveOverY' = ref false
+
+let specs =
+[
+  ('o',"orates",(set oratesNow' (not !oratesNow')),None);
+  ('y',"orates",(set saveOverY' (not !saveOverY')),None)
+]
 
 let () =
-  let args = getArgs in
-  let (b1Name,b2Name,saveBase) =
+  let args = getOptArgs specs in
+  
+  let oratesNow,   saveOverY = 
+      !oratesNow', !saveOverY' in
+
+  let b1Name,b2Name,saveBase =
   match args with
     | b1Name::b2Name::saveBase::restArgs -> (b1Name,b2Name,saveBase)
     | _ -> failwith "usage: doverlaps b1Name b2Name saveBase"
   in
   let saveSuffix = saveBase^".mlb" in
-  let osetsName  = "overs-"^saveSuffix in
-  let overxName  = "overx-"^saveSuffix in
-  let overyName  = "overy-"^saveSuffix in
+  let osetsName  = "overs-"^saveSuffix  in
+  let oratesName = "orates-"^saveSuffix in
+  let overxName  = "overx-"^saveSuffix  in
+  let overyName  = "overy-"^saveSuffix  in
   
-  leprintfln "reading buckets from %s and %s, saving bucket overlaps in %s, fraction to left in %s, to right in %s" 
-  b1Name b2Name osetsName overxName overyName;
-
+  leprintf "reading buckets from %s and %s, saving fraction to left in %s, "
+    b1Name b2Name overxName;
+    
+  if saveOverY then leprintf "fraction to the right in %s" overyName
+               else leprintf "not saving overy";
+  leprintf ", ";
+  if oratesNow then leprintfln "saving staying rates in overlaps in %s" oratesName
+               else leprintfln "saving overlaps as %s" osetsName;
+  
   let b1: day_buckets = loadData b1Name in
   let b2: day_buckets = loadData b2Name in
 
   let (osets:day_buckets),(overx:rates),(overy:rates) = bucketOverlapSetsRatios b1 b2 in
   
-  leprintfln "-- overx: --";
-  show_rates overx;
-  leprintfln "-- overy: --";
-  show_rates overy;
-  
-  saveData osets osetsName;
+  show_rates overx;  
   saveData overx overxName;
-  saveData overy overyName
+
+  if oratesNow then begin
+    let orates = Topsets.bucketDynamics osets in
+    saveData orates oratesName
+    end
+  else
+    saveData osets osetsName;
+  
+  if saveOverY then saveData overy overyName
