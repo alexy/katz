7ba49fda616009bce5b9794392ede9c827a73b52
Alexy Khrabrov <deliverable@gmail.com>
2011-02-25 18:01:35 -0500

    new run list

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,9 +1,2 @@
-../../sf.opt  ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -r 9876897 -j 1.0 -J 1.0 --glouni --fofnone urepsC0
-../../sf.opt  ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -r 45      -j 1.0 -J 1.0 --glouni --fofnone urepsC1wk ../ereps/ereps/dreps.mlb 7
-../../sf.opt  ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -r 4543    -j 1.0 -J 1.0 --glouni --fofnone urepsC2wk ../ereps/ereps/dreps.mlb 14
-../../sf.opt  ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -r 23658   -j 1.0 -J 1.0 --glouni --fofnone urepsC3wk ../ereps/ereps/dreps.mlb 21
-../../sf.opt  ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -r 1234567 -j 1.0 -J 1.0 --glouni --fofnone urepsC4wk ../ereps/ereps/dreps.mlb 28
-
-#for i in caps-*; do ../../doaranks.opt $i; done
-#for i in aranks-*; do ../../save_rbucks.opt $i; done
-
+for i j in fg5cf1cA0 fg5cf1cB0 fg5cf1cA1wk fg5cf1cB1wk fg5cf1cA2wk fg5cf1cB2wk fg5cf1cA3wk fg5cf1cB3wk fg5cf1cA4wk fg5cf1cB4wk \
+fg5cf1cB0 fg5cf1cC0 fg5cf1cB1wk fg5cf1cC1wk fg5cf1cB2wk fg5cf1cC2wk fg5cf1cB3wk fg5cf1cC3wk fg5cf1cB4wk fg5cf1cC4wk; do ../../doversets.opt rbucks-aranks-caps-$i.mlb rbucks-aranks-caps-$j.mlb $i-$j; done 
