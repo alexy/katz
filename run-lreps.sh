2be9f70f41e25123d3e8c81d04fb02492b63f13e
Alexy Khrabrov <deliverable@gmail.com>
2011-03-01 09:40:49 -0500

    run sg for rreps0d

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,6 +1,6 @@
-../../sf.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d2wk ../ereps/ereps/dreps.mlb 14
-../../sf.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d3wk ../ereps/ereps/dreps.mlb 21
-../../sf.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d4wk ../ereps/ereps/dreps.mlb 28
+../../sg.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d2wk ../ereps/ereps/dreps.mlb 14
+../../sg.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d3wk ../ereps/ereps/dreps.mlb 21
+../../sg.opt ../ereps/dstarts.mlb ../ereps/drnums/nr-dreps.mlb rreps0d4wk ../ereps/ereps/dreps.mlb 28
 
 for i in caps-*; do ../../doaranks.opt $i; done
 for i in aranks-*; do ../../save_rbucks.opt $i; done
