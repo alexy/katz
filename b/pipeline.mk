d5d31ea2e50e19ef2063322482bbba031e12f9e3
Alexy Khrabrov <deliverable@gmail.com>
2011-03-03 16:21:42 -0500

    order Xreps

--- b/pipeline.mk
+++ b/pipeline.mk
@@ -41,8 +41,44 @@ lblens:
 	mkdir -p lblens
 	mv lelb-* lblens
 
-# these can be saved right from save_rbucks, but we check afterwards:
+# these can be saved right from save_rbucks, but we like to recheck afterwards:
 rblens:
 	for i in $(BASES); do ../../dorblens.opt rbucks/rbucks-aranks-caps-$i.mlb; done
+	mkdir -p rblens
+	mv rblens-* rblens
+	
+order:
+	mkdir -p dreps
+	mv dreps-* dreps
+	mkdir -p caps
+	mv caps-* caps
+	mkdir -p skew
+	mv skew-* skew
+	mkdir -p jump
+	mv jump-* jump
+	mkdir -p norms
+	mv norms-* norms
+	mkdir -p aranks
+	mv aranks-* aranks
+	mkdir -p rbucks
+	mv rbucks-* rbucks
+	mkdir -p denums
+	mv denums-* denums
+	mkdir -p vols4
+	mv vols4-* vols4
+	mkdir -p b2br
+	mv b2br-* b2br
+	mkdir -p b2bm
+	mv b2bm-* b2bm
+	mkdir -p stars
+	mv stars-* stars
+	mkdir -p sbucks
+	mv sbucks-* sbucks
+	mkdir -p jcaps
+	mv jcaps-* jcaps
+	mkdir -p lbucks
+	mv lb-* lbucks
+	mkdir -p lblens
+	mv lelb-* lblens
 	mkdir -p rblens
 	mv rblens-* rblens
