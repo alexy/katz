a757f2fa2ed4d33d1b245ecd7845db2890ee745a
Alexy Khrabrov <deliverable@gmail.com>
2011-03-04 12:14:03 -0500

    simplify pipeline with mkdir -p handled by stages themselves, use .SECONDARY

--- b/pipeline.mk
+++ b/pipeline.mk
@@ -80,8 +80,6 @@ show:
 
 denums1:
 	for i in $(BASES); do $(SAVE_DAYS) $(DREPS_DIR)/dreps-$$i.mlb; done
-	mkdir -p    $(DENUMS_DIR)
-#	mv denums-* $(DENUMS_DIR)
 
 $(DIRS):
 	mkdir -p $@
@@ -94,8 +92,6 @@ denums2: $(DENUMS)
 
 vols1:
 	for i in $(BASES); do $(DOVOLS2) $(DENUMS_DIR)/$(DENUMS_PREFIX)-$$i.mlb $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-$$i.mlb; done
-	mkdir -p   $(VOLS4_DIR)
-#	mv vols4-* $(VOLS4_DIR)
 
 $(VOLS4): $(VOLS4_DIR)
 $(VOLS4): $(VOLS4_DIR)/vols4-$(RBUCKS_PREFIX)-%.mlb: $(DENUMS_DIR)/$(DENUMS_PREFIX)-%.mlb rbucks/$(RBUCKS_PREFIX)-%.mlb
@@ -105,10 +101,7 @@ vols2: $(VOLS4)
 
 b2br1:
 	for i in $(BASES); do $(DOB2BS) $(DREPS_DIR)/dreps-$$i.mlb $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-$$i.mlb; done
-	mkdir -p  $(B2BR_DIR)
-#	mv b2br-* $(B2BR_DIR)
 
-$(B2BR): $(B2BR_DIR)
 $(B2BR): $(B2BR_DIR)/$(B2BR_PREFIX)-%.mlb: $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-%.mlb
 	$(DOB2BS) $^
 
@@ -116,69 +109,50 @@ b2br2: $(B2BR)
 
 b2bm1:
 	for i in $(BASES); do $(DOB2BS) -i -k m $(DREPS_DIR)/dreps-$$i.mlb $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-$$i.mlb; done
-	mkdir -p  $(B2BM_DIR)
-#	mv b2bm-* $(B2BM_DIR)
 
-$(B2BM): $(B2BM_DIR)
 $(B2BM): $(B2BM_DIR)/$(B2BM_PREFIX)-%.mlb: $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-%.mlb
-	$(DOB2BS) -i -k m $^
+	$(DOB2BS) -i -k m $^ $(B2BM_DIR)
 
 b2bm2: $(B2BM)
 
 sbucks1:
 	for i in $(BASES); do $(DOSRANKS) $(DREPS_DIR)/dreps-$$i.mlb $(CAPS_DIR)/caps-$$i.mlb; done
-	mkdir -p    $(STARS_DIR)
-#	mv stars-*  $(STARS_DIR)
 	for i in $(BASES); do $(DOSTARBUCKS) $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-$$i.mlb $(STARS_DIR)/$(STARS_PREFIX)-$$i.mlb; done
-	mkdir -p    $(SBUCKS_DIR)
-#	mv sbucks-* $(SBUCKS_DIR)
 
-$(STARS):  $(STARS_DIR)
+# .INTERMEDIATE can be used instead of .SECONDARY to rm them when done
+.SECONDARY: $(STARS)
 $(STARS):  $(STARS_DIR)/$(STARS_PREFIX)-%.mlb: $(DREPS_DIR)/dreps-%.mlb $(CAPS_DIR)/caps-%.mlb
-	$(DOSRANKS) $^
+	$(DOSRANKS) $^ $(STARS_DIR)
 
-.INTERMEDIATE: $(SBUCKS)
-$(SBUCKS): $(SBUCKS_DIR)
 $(SBUCKS): $(SBUCKS_DIR)/$(SBUCKS_PREFIX)-%.mlb: $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-%.mlb $(STARS_DIR)/$(STARS_PREFIX)-%.mlb
-#$(SBUCKS): $(SBUCKS_DIR)/$(SBUCKS_PREFIX)-%.mlb: $(DREPS_DIR)/dreps-%.mlb $(CAPS_DIR)/caps-%.mlb
-	$(DOSTARBUCKS) $^
+	$(DOSTARBUCKS) $^ $(SBUCKS_DIR)
 
 sbucks2: $(SBUCKS)
 
 lblens1:
 	for i in $(BASES); do $(SAVE_CAPS) $(CAPS_DIR)/caps-$$i.mlb; done
-	mkdir -p   $(JCAPS_DIR)
-#	mv jcaps-* $(JCAPS_DIR)
 	for i in $(BASES); do $(DOCBUCKS) $(JCAPS_DIR)/jcaps-$$i.mlb; done
-	mkdir -p   $(LBUCKS_DIR)
-#	mv lb-*    $(LBUCKS_DIR) 
 	for i in $(BASES); do $(DOLBLENS) $(LBUCKS_DIR)/$(LBUCKS_PREFIX)-$$i.mlb; done
-	mkdir -p   $(LBLENS_DIR)
-#	mv lelb-*  $(LBLENS_DIR)
 
-$(JCAPS): $(JCAPS_DIR)
+.SECONDARY: $(JCAPS)
 $(JCAPS): $(JCAPS_DIR)/jcaps-%.mlb: $(CAPS_DIR)/caps-%.mlb
-	$(SAVE_CAPS) $^
+	$(SAVE_CAPS) $^ $(JCAPS_DIR)
 
-$(LBUCKS): $(LBUCKS_DIR)
+.SECONDARY: $(LBUCKS)
 $(LBUCKS): $(LBUCKS_DIR)/$(LBUCKS_PREFIX)-%.mlb: $(JCAPS_DIR)/jcaps-%.mlb
-	$(DOCBUCKS) $^
+	$(DOCBUCKS) $^ $(LBUCKS_DIR)
 
-$(LBLENS): $(LBLENS_DIR)
 $(LBLENS): $(LBLENS_DIR)/$(LBLENS_PREFIX)-%.mlb: $(LBUCKS_DIR)/$(LBUCKS_PREFIX)-%.mlb
-	$(DOLBLENS) $^
+	$(DOLBLENS) $^ $(LBLENS_DIR)
 
 lblens2: $(LBLENS)
 
 # these can be saved right from save_rbucks, but we like to recheck afterwards:
 rblens1:
 	for i in $(BASES); do $(DORBLENS) $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-$$i.mlb; done
-	mkdir -p    $(RBLENS_DIR)
-	mv rblens-* $(RBLENS_DIR)
 
-$(RBLENS): $(RBLENS_DIR)
 $(RBLENS): $(RBLENS_DIR)/$(RBLENS_PREFIX)-%.mlb: $(RBUCKS_DIR)/$(RBUCKS_PREFIX)-%.mlb
-	$(DORBLENS) $^
+	$(DORBLENS) $^ $(RBLENS_DIR)
 
 
 order: $(DIRS)
