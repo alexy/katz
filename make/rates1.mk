ff01e29aa2230af471f1e1684fdae3acfe1ae3af
Alexy Khrabrov <deliverable@gmail.com>
2011-03-10 13:35:43 -0500

    merge with rocket

--- make/rates1.mk
+++ make/rates1.mk
@@ -1,9 +1,9 @@
-include ../../tex.mk
+include ../../../make/tex.mk
 .PHONY: tex txt
 tex:
 	mkdir -p tex
 	cat $(LIST) | xargs -n5 $(TEX4RATES) -t -i $(INPUT_PATH)
-	
+
 txt:
 	mkdir -p txt
 	cat $(LIST) | xargs -n5 $(TEX4RATES) --nomatrix --precise
