a47d68bde9a93d1bafdd918615b872280f83ed6f
Alexy Khrabrov <deliverable@gmail.com>
2011-03-20 06:50:29 -0400

    defined CAMLparam2 in kendall/tau, caml__frame and memory.h are OK now

--- kendall_tau.c
+++ kendall_tau.c
@@ -1,10 +1,12 @@
 #include <caml/mlvalues.h>
+#include <caml/memory.h>
 #include <caml/alloc.h>
 #include <caml/signals.h>
 #include <caml/bigarray.h>
 #include "kendall/tau.h"
 
 value kendall_tau(value a, value b) {
+	CAMLparam2(a,b);
 	int len = Bigarray_val(a)->dim[0];
 	double ktau = kendallNlogN(Data_bigarray_val(a),Data_bigarray_val(b),len);
 	CAMLreturn (Val_double(ktau));
