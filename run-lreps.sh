b582b2e5c0c0fa15f815fcd6599bbe60153a87fa
Alexy Khrabrov <deliverable@gmail.com>
2011-04-15 22:29:01 -0400

    the version of social capital without multiplying by toOut or toIn before dividing by toTot

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,32 +1,32 @@
-#../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glomen --fofnone                               nereps0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glomen --fofnone                               nereps1wk        ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glomen --fofnone                               wereps0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glomen --fofnone                               wereps1wk        ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glouni --fofnone                               nureps0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glouni --fofnone                               nureps1wk        ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glouni --fofnone                               wureps0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glouni --fofnone                               wureps1wk        ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glocap --fofnone                               nrreps0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glocap --fofnone                               nrreps1wk        ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glocap --fofnone                               wrreps0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb --noutil       --glocap --fofnone                               wrreps1wk        ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.1         --glocap --fofnone                               nlj1c0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.1         --glocap --fofnone                               nlj1c1wk         ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.1         --glocap --fofnone                               wlj1c0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.1         --glocap --fofnone                               wlj1c1wk         ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.8 -J 0.05 --glouni --fofcap                                nfg8cf05c0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.8 -J 0.05 --glouni --fofcap                                nfg8cf05c1wk     ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.8 -J 0.05 --glouni --fofcap                                wfg8cf05c0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.8 -J 0.05 --glouni --fofcap                                wfg8cf05c1wk     ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                nfg5cf1c0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                nfg5cf1c1wk      ../ereps/ereps/dreps.mlb 7
-#../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                nfg5cf1c2wk      ../ereps/ereps/dreps.mlb 14
-#../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                nfg5cf1c3wk      ../ereps/ereps/dreps.mlb 21
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                wfg5cf1c0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                wfg5cf1c1wk      ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                wfg5cf1c2wk      ../ereps/ereps/dreps.mlb 14
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                                wfg5cf1c3wk      ../ereps/ereps/dreps.mlb 21
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni                                nfg5mf1u0
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni                                nfg5mf1u1wk      ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni                                wfg5mf1u0
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni                                wfg5mf1u1wk      ../ereps/ereps/dreps.mlb 7
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    nfg5cf1cb6f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    nfg5cf1cb6f2wk   ../ereps/ereps/dreps.mlb 14
-#../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    nfg5cf1cb6f3wk   ../ereps/ereps/dreps.mlb 21
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    wfg5cf1cb6f1wk   ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    wfg5cf1cb6f2wk   ../ereps/ereps/dreps.mlb 14
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6                    wfg5cf1cb6f3wk   ../ereps/ereps/dreps.mlb 21
 
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    nfg5mf1ub6f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    nfg5mf1ub6f2wk   ../ereps/ereps/dreps.mlb 14
-#../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    nfg5mf1ub6f3wk   ../ereps/ereps/dreps.mlb 21
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    wfg5mf1ub6f1wk   ../ereps/ereps/dreps.mlb 7
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    wfg5mf1ub6f2wk   ../ereps/ereps/dreps.mlb 14
+ ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6                    wfg5mf1ub6f3wk   ../ereps/ereps/dreps.mlb 21
  
  
---
+++ weight_divided/suds_local.ml
@@ -0,0 +1,33 @@
+(* the original reciprocal social capital *)
+
+open Common
+open Ustats
+
+let stepOut ustats from to' num res = 
+  let {outsUS =outs; totUS =tot; balUS =bal} = ustats --> from in
+  let toBal = H.find_default bal to' 0 in
+  if toBal >= 0 then res
+  else begin
+    let toSoc = getSocCap ustats to' in
+      if toSoc = 0. then res
+      else
+        let toOut = H.find_default outs to' 1 in
+        let toTot = H.find_default tot  to' 1 in
+        let term = float (num * toBal) *. toSoc /. float toTot in
+        res -. term (* the term is negative, so we sum positive *)
+  end
+  
+  
+(* we don't really have to factor out stepIn as it's not used in the simulation *)
+let stepIn ustats from to' num ((backSum,allSum) as res) =
+  let {insUS =ins; totUS =tot; balUS =bal} = ustats --> from in
+  let toSoc = getSocCap ustats to' in
+  if toSoc = 0. then res
+  else begin
+    let toIn  = H.find_default ins to' 1 in
+    let toTot = H.find_default tot to' 1 in
+    let allTerm  = float num *. toSoc /. float toTot in
+    let toBal = H.find_default bal to' 0 in
+    let backTerm = if toBal <= 0 then 0. else float toBal *. allTerm in
+    (backSum +. backTerm,allSum +. allTerm)
+  end
