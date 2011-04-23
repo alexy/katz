23f1c3a656ff2cd81c7babbd6402d8c119cb4997
Alexy Khrabrov <deliverable@gmail.com>
2011-04-23 11:11:45 -0400

    run more nreps to match wreps list

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,15 +1,5 @@
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=1                    wfg5cf1cb1f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=1                    wfg5cf1cb1f2wk   ../ereps/ereps/dreps.mlb 14
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=1                    wfg5cf1cb1f3wk   ../ereps/ereps/dreps.mlb 21
- 
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=4                    wfg5cf1cb4f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=4                    wfg5cf1cb4f2wk   ../ereps/ereps/dreps.mlb 14
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=4                    wfg5cf1cb4f3wk   ../ereps/ereps/dreps.mlb 21
- 
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=5                    wfg5cf1cb5f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=5                    wfg5cf1cb5f2wk   ../ereps/ereps/dreps.mlb 14
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=5                    wfg5cf1cb5f3wk   ../ereps/ereps/dreps.mlb 21
- 
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=7                    wfg5cf1cb7f1wk   ../ereps/ereps/dreps.mlb 7
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=7                    wfg5cf1cb7f2wk   ../ereps/ereps/dreps.mlb 14
- ../../sf.div.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=7                    wfg5cf1cb7f3wk   ../ereps/ereps/dreps.mlb 21
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                    nfg5cf1c2wk      ../ereps/ereps/dreps.mlb 14
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                    nfg5cf1c3wk      ../ereps/ereps/dreps.mlb 21
+
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6        nfg5cf1cb6f3wk   ../ereps/ereps/dreps.mlb 21
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glomen --fofuni --buckets=6        nfg5mf1ub6f3wk   ../ereps/ereps/dreps.mlb 21
