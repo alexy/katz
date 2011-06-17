f7c4dc70db5860af82fb0bed88e3e06c68c875aa
Alexy Khrabrov <deliverable@gmail.com>
2011-06-17 14:08:08 -0700

    alternate between nreps1/2 directories

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,4 +1,7 @@
  ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=7  --nouseinall      n2fg5cf1cb7f2wk   ../ereps/ereps/dreps.mlb 14
+ cd ../nreps1negall
  ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=7                    n1fg5cf1cb7f2wk   ../ereps/ereps/dreps.mlb 14
+ cd ../nreps2noall
  ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=4  --nouseinall      n2fg5cf1cb4f2wk   ../ereps/ereps/dreps.mlb 14
+ cd ../nreps1negall
  ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=4                    n1fg5cf1cb4f2wk   ../ereps/ereps/dreps.mlb 14
