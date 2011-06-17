d839845d450b1389ea864f116988fec6b139ce2c
Alexy Khrabrov <deliverable@gmail.com>
2011-06-17 09:12:01 -0700

    running with --nouseinall for nreps2

--- run-lreps.sh
+++ run-lreps.sh
@@ -1,3 +1,3 @@
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap                    n1fg5cf1c2wk      ../ereps/ereps/dreps.mlb 14
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6        n1fg5cf1cb6f2wk   ../ereps/ereps/dreps.mlb 14
- ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=1        n1fg5cf1cb1f2wk   ../ereps/ereps/dreps.mlb 14
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap              --nouseinall      n2fg5cf1c2wk      ../ereps/ereps/dreps.mlb 14
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=6  --nouseinall      n2fg5cf1cb6f2wk   ../ereps/ereps/dreps.mlb 14
+ ../../sf.neg.opt ../ereps/dstarts.mlb ../ereps/denums/denums-dreps.mlb -j 0.5 -J 0.1  --glocap --fofcap --buckets=1  --nouseinall      n2fg5cf1cb1f2wk   ../ereps/ereps/dreps.mlb 14
